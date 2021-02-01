bad80a3ea04177abee54d647601424a6b28b56d5
victor moye <victormoye09@gmail.com>
2021-02-01 10:50:58 -0300

    reogarnizando o projeto

---
+++ CursoWindowsForms/Frm_Principal.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace CursoWindowsForms
+{
+    partial class Frm_Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Principal));
+            this.Btn_Desmostracao_Key = new System.Windows.Forms.Button();
+            this.Btn_Hello_Word = new System.Windows.Forms.Button();
+            this.Btn_ValdiaCPF2 = new System.Windows.Forms.Button();
+            this.Btn_ValidaCPF = new System.Windows.Forms.Button();
+            this.Btn_ValidaSenha = new System.Windows.Forms.Button();
+            this.Btn_Mascara = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Desmostracao_Key
+            // 
+            this.Btn_Desmostracao_Key.FlatAppearance.BorderSize = 2;
+            this.Btn_Desmostracao_Key.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Desmostracao_Key.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Desmostracao_Key.Image")));
+            this.Btn_Desmostracao_Key.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_Desmostracao_Key.Location = new System.Drawing.Point(44, 72);
+            this.Btn_Desmostracao_Key.Name = "Btn_Desmostracao_Key";
+            this.Btn_Desmostracao_Key.Size = new System.Drawing.Size(153, 102);
+            this.Btn_Desmostracao_Key.TabIndex = 0;
+            this.Btn_Desmostracao_Key.Text = "Desmostração Key";
+            this.Btn_Desmostracao_Key.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Desmostracao_Key.UseVisualStyleBackColor = true;
+            this.Btn_Desmostracao_Key.Click += new System.EventHandler(this.Btn_Desmostracao_Key_Click);
+            // 
+            // Btn_Hello_Word
+            // 
+            this.Btn_Hello_Word.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Hello_Word.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Hello_Word.Image")));
+            this.Btn_Hello_Word.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_Hello_Word.Location = new System.Drawing.Point(245, 72);
+            this.Btn_Hello_Word.Name = "Btn_Hello_Word";
+            this.Btn_Hello_Word.Size = new System.Drawing.Size(153, 102);
+            this.Btn_Hello_Word.TabIndex = 1;
+            this.Btn_Hello_Word.Text = "Hello Word";
+            this.Btn_Hello_Word.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Hello_Word.UseVisualStyleBackColor = true;
+            this.Btn_Hello_Word.Click += new System.EventHandler(this.Btn_Hello_Word_Click);
+            // 
+            // Btn_ValdiaCPF2
+            // 
+            this.Btn_ValdiaCPF2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_ValdiaCPF2.Image = ((System.Drawing.Image)(resources.GetObject("Btn_ValdiaCPF2.Image")));
+            this.Btn_ValdiaCPF2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_ValdiaCPF2.Location = new System.Drawing.Point(245, 221);
+            this.Btn_ValdiaCPF2.Name = "Btn_ValdiaCPF2";
+            this.Btn_ValdiaCPF2.Size = new System.Drawing.Size(153, 93);
+            this.Btn_ValdiaCPF2.TabIndex = 2;
+            this.Btn_ValdiaCPF2.Text = "Valida CPF 2";
+            this.Btn_ValdiaCPF2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_ValdiaCPF2.UseVisualStyleBackColor = true;
+            this.Btn_ValdiaCPF2.Click += new System.EventHandler(this.Btn_ValdiaCPF2_Click);
+            // 
+            // Btn_ValidaCPF
+            // 
+            this.Btn_ValidaCPF.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_ValidaCPF.Image = ((System.Drawing.Image)(resources.GetObject("Btn_ValidaCPF.Image")));
+            this.Btn_ValidaCPF.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_ValidaCPF.Location = new System.Drawing.Point(44, 223);
+            this.Btn_ValidaCPF.Name = "Btn_ValidaCPF";
+            this.Btn_ValidaCPF.Size = new System.Drawing.Size(153, 91);
+            this.Btn_ValidaCPF.TabIndex = 3;
+            this.Btn_ValidaCPF.Text = "Valida CPF";
+            this.Btn_ValidaCPF.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_ValidaCPF.UseVisualStyleBackColor = true;
+            this.Btn_ValidaCPF.Click += new System.EventHandler(this.Btn_ValidaCPF_Click);
+            // 
+            // Btn_ValidaSenha
+            // 
+            this.Btn_ValidaSenha.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_ValidaSenha.Image = ((System.Drawing.Image)(resources.GetObject("Btn_ValidaSenha.Image")));
+            this.Btn_ValidaSenha.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_ValidaSenha.Location = new System.Drawing.Point(464, 223);
+            this.Btn_ValidaSenha.Name = "Btn_ValidaSenha";
+            this.Btn_ValidaSenha.Size = new System.Drawing.Size(120, 91);
+            this.Btn_ValidaSenha.TabIndex = 4;
+            this.Btn_ValidaSenha.Text = "Valida Senha";
+            this.Btn_ValidaSenha.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_ValidaSenha.UseVisualStyleBackColor = true;
+            this.Btn_ValidaSenha.Click += new System.EventHandler(this.Btn_ValidaSenha_Click);
+            // 
+            // Btn_Mascara
+            // 
+            this.Btn_Mascara.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Mascara.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Mascara.Image")));
+            this.Btn_Mascara.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btn_Mascara.Location = new System.Drawing.Point(464, 72);
+            this.Btn_Mascara.Name = "Btn_Mascara";
+            this.Btn_Mascara.Size = new System.Drawing.Size(120, 102);
+            this.Btn_Mascara.TabIndex = 5;
+            this.Btn_Mascara.Text = "Mascara";
+            this.Btn_Mascara.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Mascara.UseVisualStyleBackColor = true;
+            this.Btn_Mascara.Click += new System.EventHandler(this.Btn_Mascara_Click);
+            // 
+            // Frm_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 356);
+            this.Controls.Add(this.Btn_Mascara);
+            this.Controls.Add(this.Btn_ValidaSenha);
+            this.Controls.Add(this.Btn_ValidaCPF);
+            this.Controls.Add(this.Btn_ValdiaCPF2);
+            this.Controls.Add(this.Btn_Hello_Word);
+            this.Controls.Add(this.Btn_Desmostracao_Key);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Frm_Principal";
+            this.Text = "Frm_Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Desmostracao_Key;
+        private System.Windows.Forms.Button Btn_Hello_Word;
+        private System.Windows.Forms.Button Btn_ValdiaCPF2;
+        private System.Windows.Forms.Button Btn_ValidaCPF;
+        private System.Windows.Forms.Button Btn_ValidaSenha;
+        private System.Windows.Forms.Button Btn_Mascara;
+    }
+}
---
+++ CursoWindowsForms/Frm_Principal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal : Form
+    {
+        public Frm_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Desmostracao_Key_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey abir = new Frm_DemonstracaoKey();
+            abir.Show(); 
+        }
+
+        private void Btn_Hello_Word_Click(object sender, EventArgs e)
+        {
+            Frm_HelloWorld abir = new Frm_HelloWorld();
+            abir.Show();
+        }
+
+        private void Btn_Mascara_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara abir = new Frm_Mascara();
+            abir.Show();
+        }
+
+        private void Btn_ValidaCPF_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF abir = new Frm_ValidaCPF();
+            abir.Show();
+        }
+
+        private void Btn_ValdiaCPF2_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF2 abir = new Frm_ValidaCPF2();
+            abir.Show();
+        }
+
+        private void Btn_ValidaSenha_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha abir = new Frm_ValidaSenha();
+            abir.Show();
+        }
+    }
+}
